ad81a5ce84d06a00838462d556183280730759f8
woojgh <jamessalamonsen@gmail.com>
2017-04-13 19:09:40 -0700

    adding a visual representation to my data structure.

--- linkedListDataStructure.js
+++ linkedListDataStructure.js
@@ -1,6 +1,24 @@
-function linkedList(head){
-	this.head = null;
+'use strict';
+
+function Node(value, next) {
+ this.value = value;
+ this.next = next ? next : null;
 }
-linkedList.prototype.push(function(val){
-	this.head = new Node(val, this.head)
-})
+
+function LinkedList() {
+ this.head = null;
+ this.length = 0;
+}
+
+LinkedList.prototype.push = function(value) {
+ this.head = new Node(value, this.head);
+ this.length++;
+};
+LinkedList.prototype.display = function() {
+ var curr = this.head;
+ var output = '';
+ while(curr) {
+   output += curr.value + '->';
+   curr = curr.next;
+ }
+ return output;
